48c75185d2a7a2833d6fe319e13a350af12e64aa
Elphazy <52028169+Elphazy@users.noreply.github.com>
2020-03-04 12:23:56 +0300

    add
    main project

---
+++ aboutus.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Real Time Venue Booking</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >Real Time Venue Booking</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+	<li><a href = "index.php">Homepage</a></li> |		
+		<li><a href = "reservation.php">Make a reservation</a></li> |
+		<li><a href = "rulesandregulation.php">Hall Rules & Regulation</a></li> |
+		<li><a href = "aboutus.php">About us</a></li> |
+		<li><a href = "contactus.php">Contact us</a></li> |
+	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>ABOUT US</h3></strong>
+				<p style = "position:relative; float:left; width:850px;">Real Time Venue booking system is a web based system in which customers and dealers can add and book the venues based on their location,availability and area.
+                           In this system the user needs to be registered and log in.After logging in the user will search for venues by giving his/her present location in order to know the venues present.</p>
+</p>
+				<img style = "float:right;" src = "images/about.jpg" width = "250px" height = "250px" />
+				<br style = "clear:both;" />
+				<br />
+				<br />
+				<hr style = "border:1px dotted #000;" />
+				<br />
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/picture1.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Standard</h4></center>
+					<center><label>L.Nakuru hall</label> <label style = "color:red;">Php. 1,000.00</label></center>
+				</div>
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/picture5.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Meeting hall</h4></center>
+					<center>L.Magadi hall <label style = "color:red;">Php. 800.00</label></center>
+				</div>
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/picture7.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);"> Main Conference hall</h4></center>
+					<center><label>L.Victoria hall</label> <label style = "color:red;">Php. 1,900.00 </label></center>
+				</div>
+				<br style = "clear:both;"/>
+				<br />
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/picture4.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Seminar hall</h4></center>
+					<center><label>L.Elementaita hall</label> <label style = "color:red;">Php. 3,000.00 </label></center>
+				</div>
+			
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/picture6.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Network hall</h4></center>
+					<center><label>L.Naivasha hall</label> <label style = "color:red;">Php. 3,500.00 </label></center>
+				</div>
+				<br style = "clear:both;"/>
+				<br />
+				<br />
+				<strong><h3>Amenities and Services</h3></strong>
+				<ul>
+					<li><label>Function & Meeting Rooms</label></li>
+					<li><label>Free use Wifi</label></li>
+					<li><label>Business Center</label></li>
+					<li><label>Free Parking for Guest</label></li>
+					
+				</ul>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
